3623fcdf2de444258cd95033bf431b1f86e6f50c
Oleksii Chyharov <chyharov@gmail.com>
2022-07-31 21:26:22 +0300

    trasaction_history

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,8 @@ import Statistics from './Statistics/Statistics';
 import data from '../data/statistics.json';
 import FriendList from './FriendList/FriendList';
 import friends from '../data/friends.json'
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+import transactions from '../data/transaction.json';
 
 export const App = () => {
   return (
@@ -17,6 +19,7 @@ export const App = () => {
       />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
